64ee4fb7763307585d3905bbcb874bb25c59ce39
Nikolay Pylypchatin <macbook@Nick.local>
2020-11-21 18:32:12 +0200

    functional

--- templates/DetailedFloorPlanTemplate/DetailedFloorPlanTemplate.jsx
+++ templates/DetailedFloorPlanTemplate/DetailedFloorPlanTemplate.jsx
@@ -5,25 +5,31 @@ import Slider from '../../components/Slider/Slider';
 import Button from '../../components/UI/Button/Button';
 import Router from 'next/router';
 
-const DetailedFloorPlanTemplate = ({selectorPlan}) => {
+const DetailedFloorPlanTemplate = ({ selectorPlan }) => {
 
 
     return (
         <div className={styles.DetailedFloorPlanTemplate}>
             <div className={styles.DetailedFloorPlanTemplate__wrapItem}>
-                <Item 
-                    noButton
-                    data={selectorPlan}
-                />
+                {selectorPlan && (
+                    <Item
+                        noButton
+                        data={selectorPlan}
+                    />
+                )}
+
             </div>
 
             <div className={styles.DetailedFloorPlanTemplate__wrapSlider}>
-                <Slider 
-                    images={selectorPlan.images}
-                />
+                {selectorPlan && (
+                    <Slider
+                        images={selectorPlan.images}
+                    />
+                )}
+
                 <Button
                     text='Choose this floorplan!'
-                    style={{height:'70px', width:'400px'}}
+                    style={{ height: '70px', width: '400px' }}
                     onclick={() => Router.replace('/customize_lnterior')}
                 />
             </div>
