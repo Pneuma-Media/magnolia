a9d682319edfe6d0c5a9990e5af3d0d43cfff75e
Vladimir Pilipchatin <vladymyr.pylypchatin@gmail.com>
2021-01-12 14:52:46 +0200

    fixed

--- templates/CustomizeInteriorRemplate/CustomizationUnit/CustomizationUnit.jsx
+++ templates/CustomizeInteriorRemplate/CustomizationUnit/CustomizationUnit.jsx
@@ -46,8 +46,8 @@ const CustomizationUnit = ({ categoryName, optionGroups, onChange, onNext, onBac
 
     if (isAllStepsCompleted) body = (
         <div className={styles.summary}>
-            <div className={styles.summary__total}>Total: ${formatPrice(selectedPlan.price + totalCustomizationPrice + 21000)}</div>
-            <div className={styles.summary__item}>Base price:  ${formatPrice(selectedPlan.price)}</div>
+            <div className={styles.summary__total}>Total: ${formatPrice(selectedPlan?.price + totalCustomizationPrice + 21000)}</div>
+            <div className={styles.summary__item}>Base price:  ${formatPrice(selectedPlan?.price)}</div>
             <div className={styles.summary__item}>Customizations: ${formatPrice(totalCustomizationPrice)}</div>
             <div className={styles.summary__item}>Site preparation: $21,000</div>
             <div className={styles.summary__action}>
